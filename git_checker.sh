ffabcc66782c6763bf0384087610fcfbcaaff73b
TikTumanyan <tigran.r.tumanyan@gmail.com>
2026-03-28 16:20:58 +0400

    Update git_checker.sh

--- git_checker.sh
+++ git_checker.sh
@@ -3,7 +3,7 @@ if command -v git
 then                          
     echo "Git is already installed."
 else                                
-    echo "Git not found installing "
+    echo "Git not found, installing "
     sudo apt update
     sudo apt install -y git
     echo "Git is installed."
